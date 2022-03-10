6a85a389364cc28e541cf4a2d39f1f9eaa144f77
Examinati <72595444+Eximinati@users.noreply.github.com>
2022-03-10 17:50:58 +0500

    Update Support.ts

--- src/commands/General/Support.ts
+++ src/commands/General/Support.ts
@@ -23,7 +23,7 @@ export default class Command extends BaseCommand {
            MessageType.text
         ))
         const n = [
-            'https://c.tenor.com/qNi2QEbUBlEAAAAM/rikka-smile.gif'
+            'https://c.tenor.com/qNi2QEbUBlEAAAAM/rikka-smile.mp4'
         ]
         let beckylynch = n[Math.floor(Math.random() * n.length)]
         return void this.client.sendMessage(M.from, { url:beckylynch }, MessageType.video, {quoted:M.WAMessage,
